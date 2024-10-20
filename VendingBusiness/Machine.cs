51328d159650e87006471758629d96035288dc4b
Петр Брякин <petr.bryakin@gmail.com>
2024-10-20 10:32:00 +0300

    add machine properties && constructor

--- VendingBusiness/Machine.cs
+++ VendingBusiness/Machine.cs
@@ -8,6 +8,21 @@ namespace VendingBusiness
 {
     public class Machine
     {
+        public int Index { get; private set; }
+        public decimal Balance { get; private set; }
+        public double Water { get; private set; }
+        public double Coffee { get; private set; }
+        public double Milk { get; private set; }
+        public double Shugar { get; private set; }
 
+        private bool Session;
+        private decimal SessionOddMoney;
+        private decimal SessionBalance;
+        private double BarrelVolume = 12500;
+
+        public Machine(int index)
+        {
+            this.Index = index;
+        }
     }
 }
