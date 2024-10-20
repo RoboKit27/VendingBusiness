86034bf6ba8e2f74cd866723599006dd1ee56969
Петр Брякин <petr.bryakin@gmail.com>
2024-10-20 13:01:48 +0300

    add CompileError && error list

---
+++ VendingBusiness/Errors.cs
@@ -0,0 +1,14 @@
+﻿namespace VendingBusiness
+{
+    public enum Error
+    {
+        Null=-1,
+
+        FewMoney=0,
+
+        NotWater=1,
+        NotCoffee=2,
+        NotMilk=3,
+        NotShugar=4
+    }
+}
--- VendingBusiness/Machine.cs
+++ VendingBusiness/Machine.cs
@@ -15,7 +15,7 @@ namespace VendingBusiness
         public double Milk { get; private set; }
         public double Shugar { get; private set; }
 
-        private bool Session;
+        private Error ErrorStatus = Error.Null;
         private decimal SessionBalance;
         private double BarrelVolume = 12500;
 
@@ -26,30 +26,82 @@ namespace VendingBusiness
 
         private void MakeCoffee(double water, double coffee, double milk, bool shugar)
         {
-            if (this.Water >= water && this.Coffee >= coffee && this.Milk >= milk)
+            if (this.ErrorStatus == Error.Null)
             {
-                this.Water -= water;
-                this.Coffee -= coffee;
-                this.Milk -= milk;
-                if (shugar && this.Shugar >= 10)
+                if (this.Water >= water && this.Coffee >= coffee && this.Milk >= milk)
                 {
-                    this.Shugar -= 10;
+                    this.Water -= water;
+                    this.Coffee -= coffee;
+                    this.Milk -= milk;
+                    if (shugar && this.Shugar >= 10)
+                    {
+                        this.Shugar -= 10;
+                    }
+                    else
+                    {
+                        this.CompileError(Error.NotShugar);
+                    }
+                }
+                else
+                {
+                    if (this.Water < water)
+                    {
+                        this.CompileError(Error.NotWater);
+                    }
+                    else if (this.Coffee < coffee)
+                    {
+                        this.CompileError(Error.NotCoffee);
+                    }
+                    else
+                    {
+                        this.CompileError(Error.NotMilk);
+                    }
                 }
             }
         }
         private void BuyCoffee(decimal price)
         {
-            if (this.SessionBalance >= price)
+            if (this.SessionBalance >= price && this.ErrorStatus == Error.Null)
             {
                 this.SessionBalance -= price;
             }
         }
         private decimal ReturnOddMoney()
         {
-            this.Session = false;
-            decimal oddMoney = this.SessionBalance;
-            this.SessionBalance = 0;
-            return oddMoney;
+            if (this.ErrorStatus == Error.Null)
+            {
+                decimal oddMoney = this.SessionBalance;
+                this.SessionBalance = 0;
+                return oddMoney;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private void CompileError(Error errorCode)
+        {
+            this.ErrorStatus = errorCode;
+            if (errorCode == Error.FewMoney)
+            {
+                Console.WriteLine("Внесённой суммы денег не хватает! Пополните баланс!");
+            }
+            else if (errorCode == Error.NotWater)
+            {
+                Console.WriteLine("Закончилась вода!");
+            }
+            else if (errorCode == Error.NotCoffee)
+            {
+                Console.WriteLine("Закончилось кофе!");
+            }
+            else if (errorCode == Error.NotMilk)
+            {
+                Console.WriteLine("Закончилось молоко!");
+            }
+            else if (errorCode == Error.NotShugar)
+            {
+                Console.WriteLine("Закончился сахар!");
+            }
         }
 
         public void GetMoney(decimal sum)
@@ -58,10 +110,10 @@ namespace VendingBusiness
             {
                 this.Balance += sum;
                 this.SessionBalance += sum;
-            }
-            if (!this.Session)
-            {
-                this.Session = true;
+                if (this.ErrorStatus == Error.FewMoney)
+                {
+                    this.ErrorStatus = Error.Null;
+                }
             }
         }
         public decimal BuyAmericano(bool shugar)
@@ -89,6 +141,10 @@ namespace VendingBusiness
             this.Coffee = this.BarrelVolume;
             this.Milk = this.BarrelVolume;
             this.Shugar = this.BarrelVolume;
+            if (this.ErrorStatus == Error.NotWater && this.ErrorStatus == Error.NotCoffee && this.ErrorStatus == Error.NotMilk && this.ErrorStatus == Error.NotShugar)
+            {
+                this.ErrorStatus = Error.Null;
+            }
         }
 
     }
